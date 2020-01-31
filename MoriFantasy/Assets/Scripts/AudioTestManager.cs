86c49234343332e3a61f00dcd75f73d7bd3884f7
sakuriver <suzuerain@yahoo.co.jp>
2020-02-01 04:48:30 +0900

    音楽再生シーンを用意
    メインBGM再生機能を入れた

---
+++ MoriFantasy/Assets/Scripts/AudioTestManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTestManager : MonoBehaviour
+{
+
+    /// <summary>
+    /// オーディオ再生管理
+    /// </summary>
+    public AudioSource playAudioSource;
+
+    /// <summary>
+    /// メインBGMクリップ
+    /// </summary>
+    public AudioClip mainBgmClip;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Start()
+    {
+        playAudioSource = GetComponent<AudioSource>();
+        
+    }
+
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// MainBgm再生テスト
+    /// </summary>
+    public void MainBgmTest() {
+        if (!playAudioSource.isPlaying) {
+            playAudioSource.PlayOneShot(mainBgmClip);
+        }
+    }
+
+
+}
