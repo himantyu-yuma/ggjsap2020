686ba4bc99f33888ddb52b66fcc65b8adb43c99f
himantyu-yuma <himantyu.and.gato@gmail.com>
2020-02-01 03:10:21 +0900

    issue #1

---
+++ MoriFantasy/Assets/Scripts/ICharacter.cs
@@ -0,0 +1,13 @@
+﻿public interface ICharacter
+{
+    /// <summary>
+    /// ダメージ受けたときに呼び出される関数
+    /// </summary>
+    /// <param name="damage">ダメージ量</param>
+    void Damage(int damage);
+    /// <summary>
+    /// 回復されるときに呼び出される関数
+    /// </summary>
+    /// <param name="repair">回復量</param>
+    void Repair(int repair);
+}
