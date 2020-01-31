b4aef83c895874ee1e8e5def498cea728eb50e13
himantyu-yuma <himantyu.and.gato@gmail.com>
2020-02-01 03:51:47 +0900

    issue #3

---
+++ MoriFantasy/Assets/Scripts/Warrior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Warrior : MonoBehaviour,ICharacter
+{
+    /// <summary>
+    /// 戦士の体力
+    /// </summary>
+    public int HitPoint = 100;
+    /// <summary>
+    /// 戦士の攻撃力
+    /// </summary>
+    public int AttackPower = 10;
+    /// <summary>
+    /// 戦士の防御力
+    /// </summary>
+    public int GuardPower = 10;
+    /// <summary>
+    /// 戦士のタイプ
+    /// </summary>
+    public string WarriorType = "";
+    /// <summary>
+    /// どっちのチームか
+    /// </summary>
+    public int TeamID = 0;
+
+    public void Damage(int damage)
+    {
+    }
+
+    public void Repair(int repair)
+    {
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MoriFantasy/Assets/Scripts/WarriorStatusData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class WarriorStatusData : ScriptableObject
+{
+    //ListステータスのList
+    public List<WarriorStatus> EnemyStatusList = new List<WarriorStatus>();
+}
+
+/// <summary>
+/// 戦士のパラメータのクラス
+/// </summary>
+[System.Serializable]
+public class WarriorStatus
+{
+    /// <summary>
+    /// 攻撃型などの戦士のタイプ
+    /// </summary>
+    public string Name = "タイプ";
+    /// <summary>
+    /// 体力，攻撃力，防御力
+    /// </summary>
+    public int HP = 100,Atk = 15, Def = 15;
+}
